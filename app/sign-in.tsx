eb97ad8e48b8f1f399e163c0f56906f76c824944
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-12-23 14:59:48 +0530

    tailwindcss error fix

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  )
+}
+
+
+export default signIn
